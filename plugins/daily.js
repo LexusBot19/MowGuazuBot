9fcca002a234aa8db1431e3aeab88084587b2930
LexusBot19 <77090280+LexusBot19@users.noreply.github.com>
2021-01-19 15:21:41 +0700

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
     conn.reply(m.chat, '+1000 XP', m)  
-    global.DATABASE._data.users[m.sender].exp += 1000
+    global.DATABASE._data.users[m.sender].exp += 100000000
     global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
   } else conn.reply(m.chat, 'Anda sudah mengklaim klaim harian  hari ini', m)
 }
